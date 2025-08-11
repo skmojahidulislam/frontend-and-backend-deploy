8af67400b4b34edc50b6efa356d96287f085bd45
skmojahidulislam <skmojahidulislam298@gmail.com>
2025-08-11 10:12:39 +0530

    changes in backend

--- fashion-backend/index.js
+++ fashion-backend/index.js
@@ -12,7 +12,7 @@ app.get('/api/product', (req, res) => {
       { name: 'T-shirts' },
       { name: 'Shoes' },
       { name: 'Sandals' },
-      { name: 'Saree' },
+      { name: 'Sarees' },
     ]
   });
 });
